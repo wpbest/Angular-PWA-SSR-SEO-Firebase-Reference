1398ebbdfe8849056e477c71648af1af805ccfb5
William Best <wpbest@gmail.com>
2019-07-07 07:42:56 -0600

    Updated Credentials

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,13 @@
+// ToDo: Enter the Firebase credentials
 export const environment = {
   production: true,
   firebaseConfig:  {
-    apiKey: 'AIzaSyB4-1__7xskWpFEZ259MRnOhFf6BW-7al0',
-    authDomain: 'angular-pwa-ssr-seo-firebase.firebaseapp.com',
-    databaseURL: 'https://angular-pwa-ssr-seo-firebase.firebaseio.com',
-    projectId: 'angular-pwa-ssr-seo-firebase',
-    storageBucket: 'angular-pwa-ssr-seo-firebase.appspot.com',
-    messagingSenderId: '757899177099',
-    appId: '1:757899177099:web:5c33ed1a530df3b5'
+    apiKey: '...',
+    authDomain: '...',
+    databaseURL: '...',
+    projectId: '...',
+    storageBucket: '...',
+    messagingSenderId: '...',
+    appId: '...'
   }  
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,16 +2,17 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+// ToDo: Enter the Firebase credentials
 export const environment = {
   production: false,
   firebaseConfig:  {
-    apiKey: 'AIzaSyB4-1__7xskWpFEZ259MRnOhFf6BW-7al0',
-    authDomain: 'angular-pwa-ssr-seo-firebase.firebaseapp.com',
-    databaseURL: 'https://angular-pwa-ssr-seo-firebase.firebaseio.com',
-    projectId: 'angular-pwa-ssr-seo-firebase',
-    storageBucket: 'angular-pwa-ssr-seo-firebase.appspot.com',
-    messagingSenderId: '757899177099',
-    appId: '1:757899177099:web:5c33ed1a530df3b5'
+    apiKey: '...',
+    authDomain: '...',
+    databaseURL: '...',
+    projectId: '...',
+    storageBucket: '...',
+    messagingSenderId: '...',
+    appId: '...'
   }  
 };
 
